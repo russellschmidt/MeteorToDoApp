313134246b696eafd89fe37b5cf3184fdabad0d1
Russell Schmidt <russell.schmidt@me.com>
2016-06-04 11:40:20 -0700

    add ios/android android needs work

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,6 +2,8 @@ import { Template } from 'meteor/templating';
  
 import { Tasks } from '../api/tasks.js';
 
+import './task.js';
+
 import './body.html';
  
 Template.body.helpers({
@@ -9,3 +11,26 @@ Template.body.helpers({
   	return Tasks.find({});
   },
 });
+
+Template.body.events({
+	'submit .new-task'(event) {
+		//console.log(event);
+		event.preventDefault();
+
+		const target = event.target;
+		const text = target.text.value;
+
+		Tasks.insert({
+			text,
+			createdAt: new Date(),
+		});
+
+		target.text.value = '';
+	},
+});
+
+Template.body.helpers({
+	tasks() {
+		return Tasks.find({}, { sort: { createdAt: -1 } });
+	}
+})
